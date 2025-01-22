408b4b82d027622d9a6ccc2d10dda452ad647aa9
Angelo Paolo Obispo <angelopaolo.obispo@gmail.com>
2025-01-22 21:57:34 +0800

    attempts to follow ps spec, or es5 spec wherever possible

---
+++ README.md
@@ -0,0 +1,7 @@
+# ts-ps-to-jinja
+
+🖥️ [PowerShell Language Specification](https://learn.microsoft.com/en-us/powershell/scripting/lang-spec/chapter-02?view=powershell-7.4)
+
+[Ohm JS Library](https://ohmjs.org/)
+
+[Ohm Intro](https://cs.lmu.edu/~ray/notes/introohm/)
--- src/powershell.test.ts
+++ src/powershell.test.ts
@@ -1,14 +1,14 @@
-import {test} from 'uvu';
+import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 
-import {evaluate} from './powershell';
+import { evaluate } from './powershell';
 
 test('basic', () => {
   // assert.is(evaluate('(3 + 4) * 5'), 35);
-  assert.is(evaluate('3 + 4 * 5'), 23);
-  assert.is(evaluate('4 -eq 4'), true);
-  assert.is(evaluate('3 -eq false'), true);
-  assert.is(evaluate('pi'), Math.PI);
+  assert.is(evaluate('3 + 4 * 5;'), 23);
+  assert.is(evaluate('4 -eq 4;'), true);
+  assert.is(evaluate('3 -eq false;'), true);
+  // assert.is(evaluate('pi;'), Math.PI);
 });
 
 test.run();
